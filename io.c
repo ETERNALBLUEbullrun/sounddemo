afb07dbce6e8a779740eeeef81017d57bfd050cd
Bullrun <EternalblueBullrun@gmail.com>
2023-12-09 14:33:12 -0800

    Undo typos. Good errors; show "(output == cmd)".
    ?main Return -1 for (0 < ioOpen()) errors.

--- io.c
+++ io.c
@@ -18,14 +18,15 @@ const IoRet ioOpen(Io *io, const char *restrict fPath, const char *restrict mode
 	ioSetBuff(io, NULL, 0);
 	io->io = fopen(fPath, mode);
 	if(NULL == io->io) {
-		printf("\nError: NULL == fopen(\"%s\", \"%s\")\n", fPath, mode);
+		printf("\nError: (NULL == fopen(\"%s\", \"%s\"))\n", fPath, mode);
+		io->ioSz = 0;
 		return -1;
 	}
 	io->ioSz = ioSz(io->io);
 	return 0;
 }
-const IoRet ioClose(Io *io) {
-	IoRet ioRet = 0;
+/*const IoRet*/void ioClose(Io *io) {
+	/*IoRet ioRet = 0;*/
 	if(NULL != io->buff_) {
 		free(io->buff_);
 		io->buff_ = NULL;
@@ -36,16 +37,16 @@ const IoRet ioClose(Io *io) {
 	io->buff = NULL;
 	if(0 < io->io) {
 		fclose(io->io);
-	} else {
+	}/* else {
 		ioRet = -1;
-	}
+	}*/
 	io->io = 0;
-	return ioRet;
+	/*return ioRet;*/
 }
 const IoRet ioToBuff(Io *io, IoSz rSz, long offset /*= 0L*/, int whence /*= SEEK_CUR*/) {
 	void *buff = realloc(*io->buff, rSz);
 	if(NULL == buff) {
-		printf("\nError: `NULL == realloc(%p, %u)`\n", *io->buff, rSz);
+		printf("\nError: (NULL == realloc(%p, %u))\n", *io->buff, rSz);
 		return -2;
 	}
 	*io->buff = buff;
@@ -53,7 +54,7 @@ const IoRet ioToBuff(Io *io, IoSz rSz, long offset /*= 0L*/, int whence /*= SEEK
 	fseek(io->io, offset, whence);
 	io->buffSz = fread(buff, 1, rSz, io->io);
 	if(rSz != io->buffSz) {
-		printf("\nError: `%u == fread(%p, 1, %u, %p)`\n", io->buffSz, buff, rSz, io->io);
+		printf("\nError: (%u == fread(%p, 1, %u, %p))\n", io->buffSz, buff, rSz, io->io);
 		return -1;
 	}
 	return 0;
@@ -64,17 +65,17 @@ const IoRet ioFromBuff(Io *io, IoSz wSz, long offset /*= 0L*/, int whence /*= SE
 		buff = *io->buff;
 	}
 	if(NULL == buff) {
-		printf("\nError: `NULL == *%p->buff`\n", io);
+		printf("\nError: (NULL == *%p->buff)\n", io);
 		return -2;
 	}
 	if(wSz > io->buffSz) {
-		printf("\nError: `ioToBuff(%p, %u, %lu, %i)` but `%u == *%p->buffSz`\n", io, wSz, offset, whence, io->buffSz, io);
+		printf("\nError: (ioToBuff(%p, %u, %lu, %i)) but (%u == *%p->buffSz)\n", io, wSz, offset, whence, io->buffSz, io);
 		return -2;
 	}
 	fseek(io->io, offset, whence);
 	IoSz ioSz = fwrite(buff, 1, wSz, io->io);
 	if(wSz != ioSz) {
-		printf("\nError: `%u == fwrite(%p, 1, %u, %p)`\n", ioSz, buff, wSz, io->io);
+		printf("\nError: (%u == fwrite(%p, 1, %u, %p))\n", ioSz, buff, wSz, io->io);
 		return -1;
 	}
 	io->ioSz = ioSz;
@@ -88,44 +89,44 @@ const IoRet ioTest(const char *fPath) {
 	IoSz fromBuffSz = sizeof(fromBuff);
 	ioRet = ioOpen(&io, fPath, "a+");
 	if(0 < io.ioSz) {
-		printf("\nError: `%s` exists, plus `0 < ioSz`\n", fPath);
+		printf("\nError: ioTest(\"%s\"), but \"%s\" found (0 < io.ioSz)\n", fPath, fPath);
 		ioClose(&io);
 		return ioRet;
 	}
 	if(0 > ioRet) {
-		printf("\nError: `%i == ioOpen(%p, %s, %s)`\n", ioRet, &io, fPath, "a+");
+		printf("\nError: (%i == ioOpen(%p, %s, %s))\n", ioRet, &io, fPath, "a+");
 		goto exit;
 	}
 	ioSetBuff(&io, &fromBuff, fromBuffSz);
 	ioRet = ioFromBuff(&io, io.buffSz, 0, SEEK_SET);
 	if(0 > ioRet) {
-		printf("\nError: `%i == ioFromBuff(%p, %u, 0, SEEK_SET)`\n", ioRet, &io, io.buffSz);
+		printf("\nError: (%i == ioFromBuff(%p, %u, 0, SEEK_SET))\n", ioRet, &io, io.buffSz);
 		goto exit;
 	}
 	if(fromBuffSz != io.ioSz) {
-		printf("\nError: `0 == ioToBuff(%p, %u, 0, SEEK_SET)`, but `%u == %p->ioSz`.\n", &io, fromBuffSz, io.ioSz, &io);
+		printf("\nError: (0 == ioToBuff(%p, %u, 0, SEEK_SET)), but (%u == %p->ioSz).\n", &io, fromBuffSz, io.ioSz, &io);
 		goto exit;
 	}
 	ioSetBuff(&io, NULL, 0);
 	ioRet = ioToBuff(&io, io.ioSz, 0, SEEK_SET);
 	if(0 > ioRet) {
-		printf("\nError: `%i == ioToBuff(%p, %u, 0, SEEK_SET)`\n", ioRet, &io, fromBuffSz);
+		printf("\nError: (%i == ioToBuff(%p, %u, 0, SEEK_SET))\n", ioRet, &io, fromBuffSz);
 		goto exit;
 	}
 	toBuff = io.buff_;
 	if(fromBuff != *toBuff) {
-		printf("\nError: wrote %u but read %u`\n", fromBuff, *toBuff);
+		printf("\nError: wrote %u but read %u.\n", fromBuff, *toBuff);
 		goto exit;
 	}
 exit:
 	ioClose(&io);
 	ioRet = ioOpen(&io, fPath, "w");
 	if(0 > ioRet) {
-		printf("\nError: `%i == ioOpen(%p, %s, %s)`\n", ioRet, &io, fPath, "w");
+		printf("\nError: (%i == ioOpen(%p, %s, %s))\n", ioRet, &io, fPath, "w");
 		return ioRet;
 	}
 	if(0 < io.ioSz) {
-		printf("\nError: `fopen(`%s`,\"w\")` but `0 < ioSz`\n", fPath);
+		printf("\nError: (%p == fopen(\"%s\",\"w\")) but (0 < io.ioSz)\n", io.io, fPath);
 		ioRet = -1;
 	}
 	ioClose(&io);
--- sound.c
+++ sound.c
@@ -1,4 +1,6 @@
 #include <stdio.h>	/*size_t*/
+#include <stdbool.h>/*bool*/
+#include <errno.h>	/*errno*/
 #include <ctype.h>	/*uint16_t uint32_t*/
 #include <limits.h>	/*CHAR_BIT*/
 #include "sound.h"	/*RiffFormat MicrosoftRiff SubchunkId SubchunkSz MicrosoftPcm MicrosoftWave
@@ -6,52 +8,53 @@
 #include "io.h"		/*IO_CAST_TO Io IoSz IoRet IoHead IoHeader ioTest ioOpen ioClose ioToBuff*/
 const IoRet headerPcm(Io *io) {
 	const MicrosoftWave *ioWave = *io->buff;
-	printf(" %u == wave.subchunk2Sz, ", ioWave->pcm.subchunk2Sz);
+	IoRet ioRet = 0;
 	if(subchunkSzPcm != ioWave->subchunk1Sz) {
-		printf("\nError: %u == wave.subchunk1Id, should == 16 for PCM.\n", ioWave->subchunk1Sz);
-		return -1;
+		printf("\nError: (%u == MicrosoftWave.subchunk1Sz), but PCM should: (%u == MicrosoftWave.subchunk1Sz).\n", ioWave->subchunk1Sz, subchunkSzPcm);
+		ioRet = -1;
 	}
 	if(subchunkIdPcm != ioWave->pcm.subchunk2Id) {
-		printf("\nError: %x == wave.subchunk2Id, should == 'data'.\n", ioWave->pcm.subchunk2Id);
-		return -1;
+		printf("\nError: (0x%x == MicrosoftWave.subchunk2Id), but PCM should: ('data' == 0x%x == MicrosoftWave.subchunk2Id)).\n", ioWave->pcm.subchunk2Id, subchunkIdPcm);
+		ioRet = -1;
 	}
-	return 0;
+	printf(" (%u == MicrosoftWave.subchunk2Sz), ", ioWave->pcm.subchunk2Sz);
+	return ioRet;
 }
 const IoRet headerWave(Io *io) {
 	const MicrosoftWave *ioWave = NULL;
 	IoRet ioRet = 0;
 	printf(" == 'WAVE'; Microsoft (WAV) sound.\n");
 	if(0 > (ioRet = ioToBuff(io, sizeof(*ioWave), 0L, SEEK_SET))) {
-		printf("\nError: 0 > ioToBuff(%p, sizeof(*ioWave), 0L, SEEK_SET)\n", io);
+		printf("\nError: (%i == ioToBuff(%p, sizeof(*ioWave), 0L, SEEK_SET))\n", ioRet, io);
 		return ioRet;
 	}
 	ioWave = *io->buff;
 	if(subchunkIdWave != ioWave->subchunk1Id) {
-		printf("\nError: %x == wave.subchunk1Id, should == 'fmt '.\n", ioWave->subchunk1Id);
+		printf("\nError: (0x%x == MicrosoftWave.subchunk1Id), but WAVE should: ('fmt ' == 0x%x == MicrosoftWave.subchunk1Id)).\n", ioWave->subchunk1Id, subchunkIdWave);
 		return -1;
 	}
-	printf("%u == wave.subchunk1Sz", ioWave->subchunk1Sz);
-	/*printf(" %u == wave.AudioFormat", ioWave->audioFormat);*/
-	printf(", %u == wave.numChannels", ioWave->numChannels);
-	printf(", %u == wave.samplePs", ioWave->samplePs);
-	printf(", %u == wave.bytePs", ioWave->bytePs);
-	printf(", %u == wave.blockAlign", ioWave->blockAlign);
-	printf(", %u == wave.bitsPerSample", ioWave->bitsPerSample);
+	printf("(%u == MicrosoftWave.subchunk1Sz)", ioWave->subchunk1Sz);
+	/*printf(" (%u == MicrosoftWave.AudioFormat)", ioWave->audioFormat);*/
+	printf(", (%u == MicrosoftWave.numChannels)", ioWave->numChannels);
+	printf(", (%u == MicrosoftWave.samplePs)", ioWave->samplePs);
+	printf(", (%u == MicrosoftWave.bytePs)", ioWave->bytePs);
+	printf(", (%u == MicrosoftWave.blockAlign)", ioWave->blockAlign);
+	printf(", (%u == MicrosoftWave.bitsPerSample)", ioWave->bitsPerSample);
 	{
 		const IoSz bitPs = ioWave->numChannels * ioWave->samplePs * ioWave->bitsPerSample / CHAR_BIT;
 		if(bitPs != ioWave->bytePs) {
-			printf("\nError: bitPs == %u != wave.bytePs * CHAR_BIT.\n", bitPs);
+			printf("\nError: (bitPs == %u != MicrosoftWave.bytePs * CHAR_BIT).\n", bitPs);
 			return -1;
 		}
 	}
-	printf(", %u == wave.audioFormat", ioWave->audioFormat);
+	printf(", (%u == MicrosoftWave.audioFormat)", ioWave->audioFormat);
 	switch(ioWave->audioFormat) {
 	case microsoftAudioPcm:
 		printf("; PCM.");
 		return headerPcm(io);
 	default:
 		printf("; unknown.");
-		printf(" %u == wave.extraParamSz, ", ioWave->extraParamSz);
+		printf(" (%u == MicrosoftWave.extraParamSz), ", ioWave->extraParamSz);
 		return -1;
 	}
 	return 0;
@@ -61,12 +64,12 @@ const IoRet headerRiff(Io *io) {
 	IoRet ioRet = 0;
 	printf(" == 'RIFF'; Microsoft RIFF.");
 	if(0 > (ioRet = ioToBuff(io, sizeof(*ioRiff), 0L, SEEK_SET))) {
-		printf("\nError: 0 > ioToBuff(%p, sizeof(*ioRiff), 0L, SEEK_SET)\n", io);
-		return -1;;
+		printf("\nError: (%i == ioToBuff(%p, sizeof(*ioRiff), 0L, SEEK_SET))\n", ioRet, io);
+		return -1;
 	}
 	ioRiff = *io->buff;
-	printf(" %u == buffSz, ", ioRiff->chunkSz);
-	printf("0x%x == format", ioRiff->format);
+	printf(" (%u == MicrosoftRiff.buffSz), ", ioRiff->chunkSz);
+	printf("(0x%x == MicrosoftRiff.format)", ioRiff->format);
 	switch(ioRiff->format) {
 	case riffFormatWave:	/*'WAVE'*/
 		ioRet = headerWave(io);
@@ -80,7 +83,7 @@ const IoRet headerRiff(Io *io) {
 	}
 	ioRet = ioToBuff(io, io->ioSz, 0L, SEEK_SET);
 	if(0 > ioRet) {
-		printf("\nError: 0 > ioToBuff(%p, %u, 0L, SEEK_SET)\n", io, io->ioSz);
+		printf("\nError: (%i == ioToBuff(%p, %u, 0L, SEEK_SET))\n", ioRet, io, io->ioSz);
 		return ioRet;
 	}
 	return ioRet;
@@ -90,19 +93,21 @@ const IoRet headerRifx(Io *io) {
 	return -1;
 }
 const IoRet main(int argc, char **argv) {
-	Io io;
-	char *input = NULL;
 	const IoHead *inputHead = NULL;
+	const bool inputMalloc = (2 != argc);
+	char *input = NULL;
+	Io io;
 	IoRet ioRet = 0;
 	if(0 > (ioRet = ioTest("out.swp"))) {
-		printf("Error: `%i == ioTest(\"out.swp\")`", ioRet);
+		printf("Error: (%i == ioTest(\"out.swp\"))", ioRet);
+		return ioRet;
 	}
-	if(2 != argc) {
-		printf("Should '%s input.wav'. Input audio: ", argv[0]);
+	if(inputMalloc) {
+		printf("Should run '%s input.wav'. Input audio: ", argv[0]);
 		size_t inputSz = 0;
 		ioRet = getline(&input, &inputSz, stdin);
 		if(2 > ioRet) {
-			printf("Error: No input.");
+			printf("Error: No input. (getline, %i == errno)\n", errno);
 			return -1;
 		}
 		input[ioRet - 1] = '\0';
@@ -110,15 +115,19 @@ const IoRet main(int argc, char **argv) {
 		input = argv[1];
 	}
 	printf("input: \"%s\"", input);
-	ioOpen(&io, input, "ro");
+	ioRet = ioOpen(&io, input, "ro");
+	if(0 > ioRet) {
+		printf("\nError: (%i == ioOpen(%p, \"%s\", \"ro\"))\n", ioRet, &io, input);
+		goto exit;
+	}
 	printf(", %u == sz", io.ioSz);
 	ioRet = ioToBuff(&io, sizeof(*inputHead), 0L, SEEK_SET);
 	inputHead = *io.buff;
 	if(0 > ioRet) {
-		printf("\nError: 0 > ioToBuff(%p, sizeof(*inputHead), 0L, SEEK_SET)\n", &io);
+		printf("\nError: (%i == ioToBuff(%p, sizeof(*inputHead), 0L, SEEK_SET))\n", ioRet, &io);
 		goto exit;
 	}
-	printf(", 0x%x == inputHead", *inputHead);
+	printf(", (0x%x == inputHead)", *inputHead);
 	switch(*inputHead) {
 	case ioHeaderRiff:	/*'RIFF'*/
 		ioRet = headerRiff(&io);
@@ -133,10 +142,13 @@ const IoRet main(int argc, char **argv) {
 	if(0 <= ioRet) {
 		if(40<io.ioSz) io.ioSz=40;
 		for(IoSz i = 0; io.ioSz > i; ++i) {
-			printf("%x ", (IoSz)IO_CAST_TO(&io, char *)[i]);
+			printf("0x%x ", (IoSz)IO_CAST_TO(&io, char *)[i]);
 		}
 	}
 exit:
+	if(inputMalloc) {
+		free(input);
+	}
 	ioClose(&io);
 	return ioRet;
 }
