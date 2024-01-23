df5e6e264095e9aef57663188e2b00e05f92319c
Bullrun <EternalblueBullrun@gmail.com>
2024-01-23 15:29:38 -0800

    ?sound.h Include guards (allows multiple includes)

--- sound.h
+++ sound.h
@@ -4,10 +4,13 @@
  * and would go on to construct sound/audio,
  * but failed to parse small sample .wav inputs,
  * so must wav format was misunderstood somehow.*/
+#ifndef SOUND_H_
+#define SOUND_H_
 #include "io.h"		/*IoRet Io*/
 const IoRet headerPcm(Io *io);
 const IoRet headerWave(Io *io);
 const IoRet headerRiff(Io *io);
 const IoRet headerRifx(Io *io);
 const IoRet main(int argc, char **argv);
+#endif /*ndef SOUND_H_*/
 
